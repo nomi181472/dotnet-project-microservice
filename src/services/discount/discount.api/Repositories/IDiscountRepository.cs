36393539d3c3caf6d32ccdd675a3459db97d20ae
nomi181472 <nomansoomro51@gmail.com>
2021-08-22 21:37:47 -0700

    discount api

---
+++ src/services/discount/discount.api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using discount.api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace discount.api.Repositories
+{
+    public interface IDiscountRepository
+    {
+
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
