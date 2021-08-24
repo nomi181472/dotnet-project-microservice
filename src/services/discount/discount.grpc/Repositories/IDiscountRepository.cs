2dec65e5a5f1077643fcc2a5407be5ea26fdb08b
nomi181472 <nomansoomro51@gmail.com>
2021-08-23 21:41:43 -0700

    adding grpc into project

---
+++ src/services/discount/discount.grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using discount.grpc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace discount.grpc.Repositories
+{
+    public interface IDiscountRepository
+    { 
+
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
